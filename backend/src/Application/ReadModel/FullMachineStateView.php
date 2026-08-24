0a0079375c68aff69f792a567e531be864f8efd3
Xavier Palacín Ayuso <cubiczx@hotmail.com>
2026-08-24 15:18:20 +0200

    fix(docs): improve PHPDoc formatting for FullMachineStateView constructor

--- backend/src/Application/ReadModel/FullMachineStateView.php
+++ backend/src/Application/ReadModel/FullMachineStateView.php
@@ -6,8 +6,10 @@ namespace App\Application\ReadModel;
 
 final readonly class FullMachineStateView
 {
-    /** @param list<FullProductView> $products */
-    /** @param array<int, int> $changeInventory Coin value in cents => quantity */
+    /**
+     * @param list<FullProductView> $products
+     * @param array<int, int> $changeInventory Coin value in cents => quantity
+     */
     public function __construct(
         public array $products,
         public array $changeInventory,
