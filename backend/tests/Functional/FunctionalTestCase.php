0aee372c24530be29fc4615f1ee133c0104aaf62
Xavier Palacín Ayuso <cubiczx@hotmail.com>
2026-09-07 11:25:24 +0200

    Refactor: FunctionalTestCase with new methods

--- backend/tests/Functional/FunctionalTestCase.php
+++ backend/tests/Functional/FunctionalTestCase.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
-use App\Document\VendingMachineDocument;
 use App\Domain\Repository\TransactionLogRepositoryInterface;
 use App\Domain\Repository\VendingMachineRepositoryInterface;
 use App\Infrastructure\Persistence\Mongo\Document\TransactionLogDocument;
+use App\Infrastructure\Persistence\Mongo\Document\VendingMachineDocument;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -40,7 +40,7 @@ abstract class FunctionalTestCase extends WebTestCase
         $this->documentManager->getDocumentCollection(TransactionLogDocument::class)->deleteMany([]);
     }
 
-    /** @return array<string, mixed> */
+    /** @return array<array-key, mixed> */
     protected function decodeJson(): array
     {
         $content = $this->client->getResponse()->getContent();
@@ -50,5 +50,14 @@ abstract class FunctionalTestCase extends WebTestCase
         assert(is_array($decoded));
 
         return $decoded;
+    }
+
+    /** @param array<mixed> $data */
+    protected function jsonBody(array $data): string
+    {
+        $encoded = json_encode($data);
+        assert($encoded !== false);
+
+        return $encoded;
     }
 }
