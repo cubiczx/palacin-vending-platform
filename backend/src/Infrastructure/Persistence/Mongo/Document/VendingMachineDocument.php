748bc408820de47bdaf4dc2cf53462525225980d
Xavier Palacín Ayuso <cubiczx@hotmail.com>
2026-09-07 12:05:58 +0200

    fix: Add changeReturned property to VendingMachineDocument

--- backend/src/Infrastructure/Persistence/Mongo/Document/VendingMachineDocument.php
+++ backend/src/Infrastructure/Persistence/Mongo/Document/VendingMachineDocument.php
@@ -18,7 +18,7 @@ class VendingMachineDocument
 
     /** @var array<int, int> Coin value in cents (as string key) => quantity */
     #[ODM\Field(type: 'raw')]
-    public array $changeInventory = [];
+    public array $changeReturned = [];
 
     /** @var list<int> Coin values in cents, in insertion order */
     #[ODM\Field(type: 'raw')]
